1b2cef719834926060e89f0a114674d72e3df3ec
Andre Giron <agiron123@gmail.com>
2014-03-11 17:33:07 -0400

    Added Activities for each layout for the application, also added them to the AndroidManifest.xml

---
+++ src/com/example/tagalong/GroupsPageActivity.java
@@ -0,0 +1,30 @@
+package com.example.tagalong;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class GroupsPageActivity extends Activity
+{
+	  //Button myButton;
+	
+	   //onCreate is where the page is first created. Initialize things like buttons here.
+	   @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.groups_page_layout);
+	        
+	        //Example of how you initialize a button. Remember to add onClick listener later.
+	        //myButton =(Button) findViewById(R.id.button1); 
+	    }
+
+
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        // Inflate the menu; this adds items to the action bar if it is present.
+	        getMenuInflater().inflate(R.menu.main, menu);
+	        
+	        
+	        return true;
+	    }
+}
