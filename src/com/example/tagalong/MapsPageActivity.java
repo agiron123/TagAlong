66e324a73cab3946de9e499bcdbd3c2b22029e29
tli61 <tli61@gatech.edu>
2014-03-20 12:33:58 -0400

    Map stuff
    Make sure you copy and paste the google play services library folder in
    the workspace, and then reference it by adding the project and also
    configure the build path to include the jar file

--- src/com/example/tagalong/MapsPageActivity.java
+++ src/com/example/tagalong/MapsPageActivity.java
@@ -1,23 +1,48 @@
 package com.example.tagalong;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MapsPageActivity extends Activity
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsPageActivity extends FragmentActivity implements  LocationListener, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener
 {
-	  //Basically, follow stuff here: http://www.vogella.com/tutorials/AndroidGoogleMaps/article.html
 	  
-	  //Button myButton;
-	
-	   //onCreate is where the page is first created. Initialize things like buttons here.
+		private GoogleMap map;
+		private Map<String,Marker> mapMarkers = new HashMap<String,Marker>();
+		private ArrayList<Marker> newMarkerList = new ArrayList<Marker>();
+		LocationManager mLocationManager;
+		
 	   @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.map_page_layout);
-	        
-	        //Example of how you initialize a button. Remember to add onClick listener later.
-	        //myButton =(Button) findViewById(R.id.button1); 
+	        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap(); 
+	        map.setMyLocationEnabled(true);
+	        map.setMapType(GoogleMap.MAP_TYPE_NORMAL); 
+	        //Have to change coordinates below for gatech
+	        LatLngBounds gaTech = new LatLngBounds(new LatLng(29.65, -82.34), new LatLng(29.67, -82.31));
+	        map.moveCamera(CameraUpdateFactory.newLatLngZoom(gaTech.getCenter(), 11));
 	    }
 
 	    @Override
@@ -26,4 +51,28 @@ public class MapsPageActivity extends Activity
 	        getMenuInflater().inflate(R.menu.main, menu);
 	        return true;
 	    }
+
+		@Override
+		public void onConnectionFailed(ConnectionResult arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onConnected(Bundle arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onDisconnected() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onLocationChanged(Location arg0) {
+			// TODO Auto-generated method stub
+			
+		}
 }
