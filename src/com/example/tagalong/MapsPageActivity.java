7c551063770c9655a4d5af8caa3ac5307e8e1d88
tli61 <tli61@gatech.edu>
2014-03-25 18:42:39 -0400

    added map markers with info box that implements on click listener

--- src/com/example/tagalong/MapsPageActivity.java
+++ src/com/example/tagalong/MapsPageActivity.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -16,16 +17,16 @@ import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsPageActivity extends FragmentActivity implements  LocationListener, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener
+public class MapsPageActivity extends FragmentActivity implements  LocationListener, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener,OnInfoWindowClickListener
 {
 	  
 		private GoogleMap map;
@@ -41,10 +42,32 @@ public class MapsPageActivity extends FragmentActivity implements  LocationListe
 	        map.setMyLocationEnabled(true);
 	        map.setMapType(GoogleMap.MAP_TYPE_NORMAL); 
 	        //Have to change coordinates below for gatech
-	        LatLngBounds gaTech = new LatLngBounds(new LatLng(29.65, -82.34), new LatLng(29.67, -82.31));
+	        LatLngBounds gaTech = new LatLngBounds(new LatLng(33.75, -84.39), new LatLng(33.75, -84.39));
 	        map.moveCamera(CameraUpdateFactory.newLatLngZoom(gaTech.getCenter(), 11));
+	        map.addMarker(new MarkerOptions().position(new LatLng(33.47, -84.401)).title("Movie Screening").snippet("03/28/2014 8:00 PM"));
+	        map.addMarker(new MarkerOptions().position(new LatLng(33.779, -84.394)).title("All you can eat Ribs!").snippet("03/27/2014 6:00 PM"));
+	        map.addMarker(new MarkerOptions().position(new LatLng(33.777, -84.388)).title("Concert").snippet("03/29/2014 8:00 PM"));
+	        map.addMarker(new MarkerOptions().position(new LatLng(33.771816, -84.404)).title("Drinking at the Bar").snippet("03/29/2014 10:00 PM"));
+	        map.addMarker(new MarkerOptions().position(new LatLng(33.78, -84.4107)).title("Pick up Basketball").snippet("03/30/2014 2:00 PM"));
+	        
+	        map.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+				
+				@Override
+				public void onInfoWindowClick(Marker marker) {
+					Intent i = new Intent(MapsPageActivity.this, EventPageActivity.class);
+					i.putExtra("eventTitle", marker.getTitle());       			
+					startActivity(i);				
+				}
+			});
 	    }
-
+	   	
+	   @Override
+		public void onInfoWindowClick(Marker arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	   
+	   
 	    @Override
 	    public boolean onCreateOptionsMenu(Menu menu) {
 	        // Inflate the menu; this adds items to the action bar if it is present.
@@ -76,3 +99,4 @@ public class MapsPageActivity extends FragmentActivity implements  LocationListe
 			
 		}
 }
+
