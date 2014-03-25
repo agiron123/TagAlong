e9653014d903cb9a7c12e8cf358b386e4e477e14
harrisongalloway <harrisonbgalloway@gmail.com>
2014-03-25 17:45:04 -0400

    CreateEvent stuff

--- src/com/example/tagalong/CreateEventPageActivity.java
+++ src/com/example/tagalong/CreateEventPageActivity.java
@@ -11,7 +11,7 @@ public class CreateEventPageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_event_layout);
+        setContentView(R.layout.activity_create_event);
 
     }
     
